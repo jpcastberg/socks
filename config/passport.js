5a042bfa16b28559c3bc820b2d53cf59aff90fb6
John Castberg <jpcastberg@gmail.com>
2019-03-16 12:30:39 -0700

    Implement JSON web token strategy

---
+++ config/passport.js
@@ -0,0 +1,22 @@
+const { JwtStrategy, ExtractJwt } = require('passport-jwt');
+const mongoose = require('mongoose');
+
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+const opts = {};
+
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = keys.secretOrKey;
+
+module.exports = (passport) => {
+  const strategy = new JwtStrategy(opts, (jwtPayload, done) => {
+    User.findById(jwtPayload.id)
+      .then((user) => {
+        if (user) return done(null, user);
+        return done(null, false);
+      })
+      .catch(err => console.log(err));
+  });
+  passport.use(strategy);
+};
