dba7220bb79d9c47c909c030923e5406d120237e
John Castberg <jpcastberg@gmail.com>
2019-03-16 13:21:51 -0700

    Implement users api route handlers

--- routes/api/users.js
+++ routes/api/users.js
@@ -39,3 +39,41 @@ router.post('/register', (req, res) => {
     });
   });
 });
+
+// @route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+router.post('/login', (req, res) => {
+  // Form validation
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check validation
+  if (!isValid) return res.status(400).json(errors);
+  const { email, password } = req.body;
+  // Find user by email
+  User.findOne({ email }).then((user) => {
+    // Check if user exists
+    if (!user) return res.status(404).json({ emailnotfound: 'Email not found' });
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const { id, name } = user;
+        const payload = { id, name };
+        const { secretOrKey } = keys;
+        const expiresIn = 31556926; // 1 year in seconds
+        // Sign token
+        jwt.sign(payload, secretOrKey, { expiresIn }, (err, token) => {
+          if (err) throw new Error(err);
+          res.json({ success: true, token: `Bearer ${token}` });
+        });
+      } else {
+        return res
+          .status(400)
+          .json({ passwordIncorrect: 'Password incorrect' });
+      }
+    });
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 const app = express();
 
@@ -15,6 +18,13 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('MongoDB successfully connected'))
   .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require('./config/passport')(passport);
+// Routes
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 
